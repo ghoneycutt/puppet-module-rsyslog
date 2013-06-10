37de4e5034292dba9d30d7feecf65452e3726347
Brad Anderson <brad.j.anderson@ericsson.com>
2013-06-10 07:13:38 -0700

    adding in rspec tests for rsyslog

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -12,5 +12,40 @@ require 'spec_helper'
       end
 
       it { should include_class('rsyslog') }
+      
+      it {
+            should contain_package('rsyslog').with( { 'name' => 'rsyslog' } )
+            should contain_service('rsyslog').with( { 'name' => 'rsyslog' } )
+          }
+      
+      it {
+        should contain_file('syslog').with(
+          :ensure => 'file',
+          :path   => '/etc/logrotate.d/syslog',
+          :owner  => 'root',
+          :group  => 'root',
+          :mode   => '0644'
+        )
+      }
+      
+      it {
+        should contain_file('rsyslog.conf').with(
+          :ensure => 'file',
+          :path   => '/etc/rsyslog.conf',
+          :owner  => 'root',
+          :group  => 'root',
+          :mode   => '0644'
+        )
+      }
+      
+      it {
+        should contain_file('rsyslog').with(
+          :ensure => 'file',
+          :path   => '/etc/sysconfig/rsyslog',
+          :owner  => 'root',
+          :group  => 'root',
+          :mode   => '0644'
+        )
+      }
     end
   end
