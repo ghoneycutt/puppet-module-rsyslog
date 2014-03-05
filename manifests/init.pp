4bd9ac9ce0a62cc7fc448697e1b96ec874514726
Abel Chow <abel.chow@ericsson.com>
2014-03-04 17:12:05 -0800

    Add log_dir attr

--- manifests/init.pp
+++ manifests/init.pp
@@ -26,6 +26,9 @@ class rsyslog (
   $daemon_ensure            = 'running',
   $is_log_server            = 'false',
   $log_dir                  = '/srv/logs',
+  $log_dir_owner            = 'root',
+  $log_dir_group            = 'root',
+  $log_dir_mode             = '0700',
   $remote_template          = '%HOSTNAME%/%$YEAR%-%$MONTH%-%$DAY%.log',
   $remote_logging           = 'false',
   $spool_dir                = '/var/spool/rsyslog',
@@ -117,11 +120,11 @@ class rsyslog (
 
       common::mkdir_p { $log_dir: }
 
-      file { $log_dir:
+      file { 'log_dir':
         ensure  => directory,
-        owner   => 'root',
-        group   => 'root',
-        mode    => '0750',
+        owner   => $log_dir_owner,
+        group   => $log_dir_group,
+        mode    => $log_dir_mode,
         require => Common::Mkdir_p[$log_dir],
       }
     }
