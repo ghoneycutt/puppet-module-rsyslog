f2f4b0bb7fadd5daf0f25ee11dee7b8caa8cd973
Phil Friderici <phil.friderici@ericsson.com>
2017-02-21 09:46:04 +0100

    Fix facts redirection

--- lib/facter/rsyslog_version.rb
+++ lib/facter/rsyslog_version.rb
@@ -2,7 +2,7 @@
 
 Facter.add("rsyslog_version") do
   setcode do
-    test_exists = "rsyslogd -v 2>&1 >/dev/null ; echo $?"
+    test_exists = "rsyslogd -v >/dev/null 2>&1 ; echo $?"
     if Facter::Util::Resolution.exec(test_exists) == '0'
       cmd = "rsyslogd -v | grep -Eo [0-9]+\.[0-9]+\.[0-9]+"
       response = Facter::Util::Resolution.exec(cmd)
